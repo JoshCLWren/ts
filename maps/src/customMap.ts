e01caca02fc7ce0a1850fb793a416bf193f21507
Josh Wren <josh.wren@life.church>
2020-09-21 09:14:31 -0500

    Stop maps tutorial and finished React-Quiz

---
+++ maps/src/customMap.ts
@@ -0,0 +1,28 @@
+import { User } from './User';
+import { Company } from './Company';
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 4,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addUserMaker(user: User): void {
+    new this.google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {}
+}
---
+++ maps/src/index.ts
@@ -0,0 +1,13 @@
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './customMap';
+
+const user = new User();
+
+console.log(user);
+
+const company = new Company();
+
+console.log(company);
+
+new CustomMap('map');
